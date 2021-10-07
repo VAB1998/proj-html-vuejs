6f7a33546257b1e4a9080230d5089416d13c9037
VAB1998 <blanco.vincenzo@outlook.com>
2021-10-07 11:08:49 +0200

    Jumbotron Completed

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,64 @@
+<template>
+    <section id="jumbotron">
+        <div class="container h-100">
+            <div class="row align-items-center h-100">
+                    <div class="col-4">
+                    <h6>fresh & tasty bakery every dy</h6>
+                    <h2>The Perfect Fresh Bread</h2>
+                    <p>Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl varius. Aliquam erat volutpat. Nullam quis sagittis nibh ac nulla id orci tempor.</p>
+                    <a href="https://avada.theme-fusion.com/bakery/shop/">Explore our products</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: 'Jumbotron',
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+#jumbotron{
+    //Style
+    background-image: url('../assets/images/hero-header-bakery.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    //Spacing and Dimensions
+    height: 900px;
+
+
+    h6{
+        //Style
+        color: #676371;
+        text-transform: uppercase;
+        font-size: .8rem;
+    }
+
+    h2{
+        @include title_style;
+        font-size: 3.3rem;
+    }
+
+    p{
+        color: #676371;
+        margin: 30px 0;
+    }
+
+    a{  
+        @include light_link_button;
+
+        &:hover{
+            @include dark_link_button;
+        }
+    }
+}
+</style>
