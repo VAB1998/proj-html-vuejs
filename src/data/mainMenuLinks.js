31b420a7ca27175131fde7b0bd7c6af0506d86b4
VAB1998 <blanco.vincenzo@outlook.com>
2021-10-07 09:04:36 +0200

    Added Dynamic links to the Navbar

---
+++ src/data/mainMenuLinks.js
@@ -0,0 +1,38 @@
+export default [
+
+    {
+        text: 'home',
+        url: '#home',
+        current: true
+    },
+    {
+        text: 'shop',
+        url: '#shop',
+        current: false
+    },
+    {
+        text: 'about',
+        url: '#about',
+        current: false
+    },
+    {
+        text: 'gallery',
+        url: '#gallery',
+        current: false
+    },
+    {
+        text: 'locations',
+        url: '#locations',
+        current: false
+    },
+    {
+        text: 'journal',
+        url: '#journal',
+        current: false
+    },
+    {
+        text: 'contact',
+        url: '#contact',
+        current: false
+    }
+]
---
+++ src/data/socialLinks.js
@@ -0,0 +1,23 @@
+export default [
+    
+    {
+        icon: 'fab fa-instagram',
+        url: '#instagram',
+    },
+    {
+        icon: 'fab fa-twitter',
+        url: '#twitter',
+    },
+    {
+        icon: 'fab fa-facebook-f',
+        url: '#facebook',
+    },
+    {
+        icon: 'fab fa-pinterest-p',
+        url: '#pinterest',
+    },
+    {
+        icon: 'fab fa-youtube',
+        url: '#youtube',
+    },
+]
