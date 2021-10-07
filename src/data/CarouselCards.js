3e70182a46e7b690e21b07a34bb99f78ea970327
VAB1998 <blanco.vincenzo@outlook.com>
2021-10-07 16:48:34 +0200

    Created Carousel Cards Data

---
+++ src/data/CarouselCards.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title : 'Choco Chips Cookies',
+        price : '$18.00 - $32.00',
+        url : '#',
+        imageSource : require('../assets/images/choco-chip-cookies-400x510.jpg')
+    },
+    {
+        title : 'Strawberry Jam Cookies',
+        price : '$36.00 - $60.00',
+        url : '#',
+        imageSource : require('../assets/images/strawberry-jam-cookies-400x510.jpg')
+    },
+    {
+        title : 'Straweberry Donut',
+        price : '$24.00 - $48.00',
+        url : '#',
+        imageSource : require('../assets/images/strawberry-donut-400x510.jpg')
+    },
+    {
+        title : 'Perfect Macarons',
+        price : '$32.00 - $56.00',
+        url : '#',
+        imageSource : require('../assets/images/perfect-macarons-400x510.jpg')
+    }
+]
