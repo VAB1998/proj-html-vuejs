968af27d06fa2cb6fa52fc0921cd28a692dd8de9
VAB1998 <blanco.vincenzo@outlook.com>
2021-10-07 11:09:40 +0200

    Added OurProducts component

---
+++ src/components/OurProducts.vue
@@ -0,0 +1,17 @@
+<template>
+    <section id="our_products"></section>
+</template>
+
+<script>
+
+export default {
+    name: 'OurProducts',
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+</style>
